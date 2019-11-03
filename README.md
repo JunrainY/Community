4f93246d72d678d515ffc03b9b06453526a270ea
JunrainY <1240275728@qq.com>
2019-11-03 12:42:30 +0800

    add indexController

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 ## Yl的第一个spring
 
 ## 资料
-https://spring.io/guides/
-https://spring.io/guides/gs/serving-web-content/
+[](https://spring.io/guides/)
+[]https://spring.io/guides/gs/serving-web-content/
 
 ## 先放暂存    git add . "黄色"
 ## 再放入      git status "红色"
+## 备注          git commit -m " ***"
 ## 更改后再追加 git commit  --amend --no-edit
+如果出现! [rejected] master -> master (non-fast forward)
+1、git pull origin master --allow-unrelated-histories //把远程仓库和本地同步，消除差异
+
+2、重新git add .和git commit -m ""相应文件
+
+3、git push origin master
+## https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
--- src/main/java/com/yl/community/controller/IndexController.java
+++ src/main/java/com/yl/community/controller/IndexController.java
@@ -1,20 +1,26 @@
 package com.yl.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @quthor yl
  * @create 2019-11-01 16:29
+ * 1注解controller
+ * 2@GetMapping("/")的/为默认，直接返回路径 ，即index.html
  */
 @Controller
-public class HelloController {
+public class IndexController {
+    @GetMapping("/")
+    public String index() { return "index"; }
+}
+
+/**
+public class IndexController {
     @GetMapping("/hello")          //reguestparam请求参数
     public String hello(@RequestParam(name ="name") String name, Model model){
         model.addAttribute("name" ,name);//浏览器传来的值放入moder
         return  "hello";
 
     }
-}
+}*/
